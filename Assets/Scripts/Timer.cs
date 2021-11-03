ffc361397863a59eaa0a06e4e135b4e4a077df7f
Benjamin <b_vu6@u.pacific.edu>
2021-11-03 14:15:44 -0700

    time is displayed

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI; 
 public class Timer : MonoBehaviour
 {
+    public float timeValue = 90;
+
+    public Text timerText; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime; 
+        }
+        else
+        {
+            timeValue = 0; 
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0; 
+        }
+
+        float min = Mathf.FloorToInt(timeToDisplay/60);
+        float secs = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", min, secs); 
     }
 }
